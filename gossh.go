962bbe5bc6f29006d865a2142e74c3213c5b7c3f
bingoo <bingoo.huang@gmail.com>
2019-01-10 07:12:52 +0800

    refactor gossh

--- gossh.go
+++ gossh.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/mitchellh/go-homedir"
@@ -62,29 +63,36 @@ func init() {
 }
 
 func main() {
-	client := CreateClient(usr, host, port, scripts, password)
+	client, err := CreateClient(usr, host, port, password)
+	if err != nil {
+		log.Fatalf("unable to connect: %v", err)
+	}
 	defer client.Close()
 
 	scriptLines := SplitScriptLines(scripts)
 	for _, scriptLine := range scriptLines {
 		fmt.Println(prompt, scriptLine)
-		fmt.Print(prompt + " ")
-		RunScript(client, scriptLine)
+		out, err := RunScript(client, scriptLine)
+		if err != nil {
+			log.Fatalf("RunScript %s error %v", scriptLine, err)
+		}
+		fmt.Println(prompt, out)
 	}
 }
 
-// CreateClient from
-func CreateClient(usr, host string, port int, scripts, password string) *ssh.Client {
-	var auth ssh.AuthMethod
-	if password != "" {
-		auth = ssh.Password(password)
-	} else {
-		auth = CreatePublickKey()
+// CreateClient with user(optional), host, port and password(optional)
+// https://stackoverflow.com/questions/35450430/how-to-increase-golang-org-x-crypto-ssh-verbosity
+// As a quick hack you can open $GOPATH/golang.org/x/crypto/ssh/mux.go file,
+// change const debugMux = false to const debugMux = true and recompile your program.
+func CreateClient(user, host string, port int, password string) (*ssh.Client, error) {
+	auth, err := createAuth(password)
+	if err != nil {
+		return nil, err
 	}
 
 	config := &ssh.ClientConfig{
-		User:            usr,
-		Timeout:         10 * time.Second,
+		User:            user,
+		Timeout:         3 * time.Second,
 		Auth:            []ssh.AuthMethod{auth},
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 	}
@@ -92,14 +100,36 @@ func CreateClient(usr, host string, port int, scripts, password string) *ssh.Cli
 	// Connect to the remote server and perform the SSH handshake.
 	client, err := ssh.Dial("tcp", host+":"+strconv.Itoa(port), config)
 	if err != nil {
-		log.Fatalf("unable to connect: %v", err)
+		return nil, err
 	}
-	return client
+
+	return client, nil
+}
+
+func createAuth(password string) (ssh.AuthMethod, error) {
+	var err error
+	if password != "" {
+		return ssh.Password(password), nil
+	}
+
+	auth, err := CreatePublickKey()
+	if err != nil {
+		return nil, err
+	}
+	if auth != nil {
+		return auth, nil
+	}
+
+	return nil, errors.New("Please use password or auto sshed by ~/.ssh/id_rsa")
 }
 
 // CreatePublickKey from ~/.ssh/id_rs
-func CreatePublickKey() ssh.AuthMethod {
+func CreatePublickKey() (ssh.AuthMethod, error) {
 	file, _ := homedir.Expand("~/.ssh/id_rsa")
+
+	if _, err := os.Stat(file); os.IsNotExist(err) {
+		return nil, nil
+	}
 
 	// A public key may be used to authenticate against the remote
 	// server by using an unencrypted PEM-encoded private key file.
@@ -108,30 +138,30 @@ func CreatePublickKey() ssh.AuthMethod {
 	// can be used to decrypt it.
 	key, err := ioutil.ReadFile(file)
 	if err != nil {
-		log.Fatalf("unable to read private key: %v", err)
+		return nil, err
 	}
 
 	// Create the Signer for this private key.
 	signer, err := ssh.ParsePrivateKey(key)
 	if err != nil {
-		log.Fatalf("unable to parse private key: %v", err)
+		return nil, err
 	}
+
 	// Use the PublicKeys method for remote authentication
-	return ssh.PublicKeys(signer)
+	return ssh.PublicKeys(signer), nil
 }
 
 // RunScript in shell session from client
-func RunScript(client *ssh.Client, scriptLine string) {
+func RunScript(client *ssh.Client, scriptLine string) (string, error) {
 	// create session
 	session, err := client.NewSession()
 	if err != nil {
-		log.Fatalf("unable to NewSession: %v", err)
+		return "", err
 	}
 	defer session.Close()
 
-	session.Stdout = os.Stdout
-	session.Stderr = os.Stderr
-	session.Run(scriptLine)
+	out, err := session.CombinedOutput(scriptLine)
+	return string(out), err
 }
 
 // SplitScriptLines scripts to lines, ignoring comments or blank lines and auto join lines end with \
