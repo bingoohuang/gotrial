7802d7a5a784e1f48b80f211d6ba68a3fc181c75
bingoo <bingoo.huang@gmail.com>
2019-04-14 17:07:26 +0800

    refactor code

--- reflect/main.go
+++ reflect/main.go
@@ -30,16 +30,13 @@ func main() {
 	jso := `{"results":[{"statement_id":0,"series":[{"name":"mem","tags":{"host":"beta-bq"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",6780.0203125,86.67115414384232,898.2842447916667,11.483052949234583]]},{"name":"mem","tags":{"host":"beta-ca"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",1954.0084635416667,24.979122733690968,5708.663671875,72.97686431033614]]},{"name":"mem","tags":{"host":"beta-hetong"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",8579.579557291667,54.0070803835492,7180.895052083333,45.202585244878705]]},{"name":"mem","tags":{"host":"beta-qianming"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",1510.322265625,39.84152728988894,2131.5911458333335,56.230281934207206]]},{"name":"mem","tags":{"host":"beta-qianzhang"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",4243.741015625,54.24998389575031,3420.365234375,43.72433619281555]]},{"name":"mem","tags":{"host":"tencent-beta02"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",5941.044270833333,37.3938036913039,10135.829947916667,63.7963997645268]]},{"name":"mem","tags":{"host":"tencent-beta03"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",8451.113802083333,53.190260751669065,7655.479947916667,48.182640080941866]]},{"name":"mem","tags":{"host":"tencent-beta04"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",870.2723958333333,22.947172035845103,2759.5984375,72.76455095938637]]},{"name":"mem","tags":{"host":"tencent-beta16"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",2468.683984375,31.55795608832669,5218.279427083334,66.70689082070024]]},{"name":"mem","tags":{"host":"tencent-beta17"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",11059.716666666667,69.61914660821645,4796.33125,30.192137695690224]]},{"name":"mem","tags":{"host":"tencent-beta18"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",2899.252864583333,18.250332835565583,12820.065234375,80.70025914577543]]},{"name":"mem","tags":{"host":"tencent-beta19"},"columns":["time","available","available_percent","used","used_percent"],"values":[["2019-04-10T00:30:00Z",7888.863411458334,49.65913277596132,8042.551302083333,50.62657345385657]]}]}]}`
 
 	type MemSeries struct {
-		Host             string    `iql:"host/ip"`
-		Time             time.Time `iql:"time"`
-		Available        float32   `iql:"available"`
-		AvailablePercent float32   `iql:"available_percent"`
-		Used             float32   `iql:"used"`
-		UsedPercent      float32   `iql:"used_percent"`
+		Host                        string `iql:"host/ip"`
+		Time                        time.Time
+		Available, AvailablePercent float32
+		Used, UsedPercent           float32
 	}
 
 	var memSeries []MemSeries
-
 	ParseIqlResults(jso, func(m map[string]interface{}) {
 		ms := MemSeries{}
 		Map2Struct(m, &ms)
@@ -47,12 +44,11 @@ func main() {
 	})
 
 	fmt.Println(memSeries)
-
 }
 
 // UnmarshalError return error from errMsg
 func UnmarshalError(errMsg *json.RawMessage) error {
-	var s string
+	s := ""
 	_ = json.Unmarshal(*errMsg, &s)
 	return errors.New(s)
 }
