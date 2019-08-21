a966ba94fcb45dcee708081d06799abfd01d33f5
bingoohuang <bingoo.huang@gmail.com>
2019-08-21 12:59:19 +0800

    Update gohttpd.go

--- cmd/gohttpd/gohttpd.go
+++ cmd/gohttpd/gohttpd.go
@@ -3,9 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/sirupsen/logrus"
+
 	"log"
 	"net/http"
+	_ "net/http/pprof"
 	"strconv"
+	"time"
 )
 
 var (
@@ -19,6 +23,14 @@ func init() {
 }
 
 func main() {
+	go func() {
+		if err := http.ListenAndServe(":6060", nil); err != nil {
+			logrus.Fatalf("error %v", err)
+		}
+	}()
+
+	http.HandleFunc("/say", sayHandler)
+	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/error", errorHandler)
 	http.HandleFunc("/ui/index.html", uiStatusHandler)
 	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(port), nil))
@@ -26,6 +38,15 @@ func main() {
 
 func errorHandler(w http.ResponseWriter, r *http.Request) {
 	http.Error(w, "my own error message", http.StatusInternalServerError)
+}
+
+func sayHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hi there, say I love you!")
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	time.Sleep(5 * time.Second)
+	fmt.Fprintf(w, "Hi there, I love you!")
 }
 
 func uiStatusHandler(w http.ResponseWriter, r *http.Request) {
