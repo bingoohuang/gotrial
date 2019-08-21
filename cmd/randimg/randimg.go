91bf78bde32198cb0ef27acbc97fad17b983c29c
bingoohuang <bingoo.huang@gmail.com>
2019-08-21 13:48:33 +0800

    refactor size usage

--- cmd/randimg/randimg.go
+++ cmd/randimg/randimg.go
@@ -4,26 +4,38 @@ import (
 	"flag"
 	"fmt"
 
+	"github.com/dustin/go-humanize"
+
 	"github.com/bingoohuang/golang-trial/randimg"
 	"github.com/bingoohuang/gou/ran"
 )
 
 var (
-	width  int
-	height int
-	picfmt string
-	fixMib int64
-	many   int
+	width     int
+	height    int
+	picfmt    string
+	fixedSize uint64
+	many      int
 )
 
 func init() {
 	flag.IntVar(&width, "w", 640, "picture width")
 	flag.IntVar(&height, "h", 320, "picture height")
-	flag.Int64Var(&fixMib, "m", 0, "fixed size(MiB)")
+	fixedSizeStr := flag.String("s", "", "fixed size(eg. 44kB, 17MB)")
 	flag.IntVar(&many, "i", 1, "how many pictures to create")
 	flag.StringVar(&picfmt, "f", "png", "picture format(png/jpg)")
 
 	flag.Parse()
+
+	if *fixedSizeStr == "" {
+		fixedSize = 10 << 20 // 10MiB
+	} else {
+		var err error
+		fixedSize, err = humanize.ParseBytes(*fixedSizeStr)
+		if err != nil {
+			panic("illegal fixed size " + err.Error())
+		}
+	}
 }
 
 func main() {
@@ -32,8 +44,9 @@ func main() {
 	for i := 0; i < many; i++ {
 		randText := fmt.Sprintf("%d", s+i)
 		fileName := fmt.Sprintf("%d.%s", s, picfmt)
-		randimg.GenerateRandomImageFile(width, height, randText, fileName, fixMib<<20)
+		randimg.GenerateRandomImageFile(width, height, randText, fileName, int64(fixedSize))
 
-		fmt.Println(fileName, "with width", width, "height", height, "randText", randText, "generated!")
+		fmt.Println(fileName, "with width", width, "height", height, "randText", randText,
+			"fixedSize", humanize.IBytes(fixedSize), "generated!")
 	}
 }
--- go.mod
+++ go.mod
@@ -3,25 +3,16 @@ module github.com/bingoohuang/golang-trial
 go 1.12
 
 require (
-	github.com/bingoohuang/gonet v0.0.0-20190729063044-0a2a8ec96e17 // indirect
 	github.com/bingoohuang/gou v0.0.0-20190724062522-59c35e658334
-	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/golang/protobuf v1.3.2 // indirect
-	github.com/json-iterator/go v1.1.7 // indirect
+	github.com/dustin/go-humanize v1.0.0
 	github.com/konsorten/go-windows-terminal-sequences v1.0.2 // indirect
-	github.com/lestrrat-go/strftime v0.0.0-20190725011945-5c849dd2c51d // indirect
-	github.com/mattn/go-isatty v0.0.9 // indirect
+	github.com/kr/pretty v0.1.0 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pbnjay/pixfont v0.0.0-20190130005054-401bb7c6aee2
 	github.com/sirupsen/logrus v1.4.2
-	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/struCoder/pidusage v0.1.2
-	github.com/tebeka/strftime v0.1.3 // indirect
-	github.com/ugorji/go v1.1.7 // indirect
 	golang.org/x/crypto v0.0.0-20190820162420-60c769a6c586
-	golang.org/x/net v0.0.0-20190813141303-74dc4d7220e7 // indirect
-	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/tools v0.0.0-20190820205717-547ecf7b1ef1 // indirect
+	golang.org/x/sys v0.0.0-20190813064441-fde4db37ae7a // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
